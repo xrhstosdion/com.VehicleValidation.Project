e03636f10ac367799c657ac30b255b86f01a838b
ckaridis89 <ckaridis89@gmail.com>
2017-09-19 18:08:42 +0300

    Project 1

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,50 +1,78 @@
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MainProgram {
 
-    public static void main (String [] args ){
+    public static void main(String[] args) {
 
-        // diavasma arxeiou CSV
-        String csvFile = "/Users/chris/Desktop/#Projects/com.VehicleValidation.Project/csv.csv";
+        /* diavasma arxeiou CSV
+          Diabazei to arxeio CSV, me xorismenh thn kathe timh me ;
+         */
+        String csvFile = "C:/Users/ekped/Desktop/Projects/com.VehicleValidation.Project/csvv.csv";
         String line = "";
-        String cvsSplitBy = ",";
+        String cvsSplitBy = ";";
+        String expir = "VALID";
 
-        try (BufferedReader br = new BufferedReader ( new FileReader ( csvFile ) )) {
+        LocalDate localDate = LocalDate.now();
+
+        Date datenow = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        System.out.println(localDate);
+        System.out.println(datenow);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+
+            /*
+              Epanalipsh gia na apothikevei se pinaka tis times apo to CSV
+             */
 
             while ((line = br.readLine()) != null) {
+
+                /*
+                  Epanalipsi oso yparxoun grammes sto CSV
+                 */
 
                 // use comma as separator
                 String[] info = line.split(cvsSplitBy);
 
-                //String daten = info[3];
-                String daten = "02/03/2011";
+                //Date conversion
+                /* Eisagei thn hmerominia me sygkekrimeno format
+                 */
+                DateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+                Date date = df.parse(info[3]);
 
+                // Elegxos an h hmerominia einai prin thn shmerinh hmerominia
+                 if (date.before(datenow)){
+                    expir ="EXPIRED";
+                }
 
-                //Date conversion === NOT WORKING ===
-                SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-                Date date = df.parse(daten);
-                Date dateOfInsurance = df.parse ( daten ); // Here you have date stored
-
-                // ektyposh olwn twn stoixeiwn gia dokimh
+                /* ektyposh olwn twn stoixeiwn gia dokimh
+                 */
                 System.out.println("The owner is: " + info[0] + " the brand is: " + info[1] +
-                        " the model is: " + info[2] + " it was registered at: " + dateOfInsurance + " for " + info[4] + " months.");
+                        " the model is: " + info[2] + " it was registered at: "+
+                        (new SimpleDateFormat("MM-dd-yyyy").format(date))
+                        + " for " + info[4] + " months. " + expir );
 
             }
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
 
-        }
-
-
-
-
     }
+
+
+}
+
+
 
