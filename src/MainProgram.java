e0a1c7d8a67a1d441de5d6c7589e46a0df1c4a08
xrhstosdion <xrhstosdion@hotmail.com>
2017-09-21 20:50:21 +0300

    try catch

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -3,35 +3,45 @@ import java.util.Scanner;
 public class MainProgram {
 
     public static void main ( String[] args ) {
-
-        int input = 0 ;
+        String input;
+        int i =0;
         do {
 
             // epanalipsh gia na doulepsei panw apo mia askhsh
             // edw ksekinaei to programma. zhtaei ton arithmo ths askishs
-            System.out.println ( "===========================" );
-            System.out.println ( "=== OUR AWESOME PROJECT ===" );
-            System.out.println ( "PLEASE SELECT AN OPERATION" );
-            System.out.println ( "1. LicensePlatesValidator" );
-            System.out.println ( "2. DateValidator" );
-            System.out.println ( "3. ----" );
-            System.out.println ( "0. TO EXIT" );
-            System.out.println ( "===========================" );
+            System.out.println("===========================");
+            System.out.println("=== OUR AWESOME PROJECT ===");
+            System.out.println("PLEASE SELECT AN OPERATION");
+            System.out.println("1. LicensePlatesValidator");
+            System.out.println("2. DateValidator");
+            System.out.println("3. ----");
+            System.out.println("0. TO EXIT");
+            System.out.println("===========================");
 
             // edw diavazei ton arithmo pou tou dwsame
-            Scanner scan = new Scanner ( System.in );
+            Scanner scan = new Scanner(System.in);
 
             // h metablith input tha pernei to apotelesma kai tha fortonei to katalilo operation
-            input = scan.nextInt ();
+            input = scan.next();
+            try {
+                i = Integer.parseInt(input);
+            } catch (NumberFormatException e){
+                System.out.println("wrong is not valid number. try again");
+            }
+            if (i==0 || i>3){
+                System.out.println("not in 1-3");
+            }
+
+
 
             // Se kathe epilogh fortonetai h antistoixh klash
             switch (input) {
 
-                case 1: //LicensePlatesValidator
+                case "1": //LicensePlatesValidator
                     new LicensePlatesValidator();
                     break;
 
-                case 2: // DateValidator
+                case "2": // DateValidator
                     new DateValidator ();
                     break;
 
@@ -40,7 +50,7 @@ public class MainProgram {
         }
 
         // Ta parapanw tha simvenoun mexri o xrhsths na eisagei 0
-        while (input != 0);
+        while (i < 1 || 1 > 3);
         System.out.println ( "Good Bye!!" );
     }
 }
