23af24b463b8d1fd8615e17710385c4dce529e4c
ckaridis89 <ckaridis89@gmail.com>
2017-09-25 16:54:50 +0300

    .

--- src/NewDateValidator.java
+++ src/NewDateValidator.java
@@ -18,41 +18,49 @@ public class NewDateValidator {
         String filePath = new File("VehiclesData.csv").getAbsolutePath();
         System.out.println("EDW" + filePath);
 
-        String csvFile = filePath; // <- Default path gia to CSV arxeio
         String line = ""; // <- Diaxorizei thn kathe grammh sto CSV arxeio
         String cvsSplitBy = ";"; // <- Orizei me poio simvolo sto CSV arxeio pername sthn epomenh eggrafh
 
+        ////////////////////////////////////
         // Metatroph shmerinhs hmerominias se symvath morfh
         LocalDate localDate = LocalDate.now ();//For reference <- apothikefsh shmerinhs hmeorminias se metablith
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern ( "dd/MM/yyyy" );// <- metatroph se ayto to fortmat
-        String formattedDateNow = localDate.format ( formatter ); // <-
+
+        DateFormat df = new SimpleDateFormat ( "dd/MM/yyyy", Locale.ENGLISH );
+
+        ////////////////////////// LOCAL DATE
+        Date dateNow = null;
+        try {
+            dateNow = df.parse ( localDate.format ( formatter ) );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        /////////////////////////
 
         //Epanalipsh gia na apothikevei se pinaka tis times apo to CSV
-        try (BufferedReader br = new BufferedReader ( new FileReader ( csvFile ) )) {
+        try (BufferedReader br = new BufferedReader ( new FileReader ( filePath ) )) {
 
             //Date conversion
             //Eisagei thn hmerominia me sygkekrimeno format
-            DateFormat df = new SimpleDateFormat ( "dd/MM/yyyy", Locale.ENGLISH );
+
 
             while ((line = br.readLine ()) != null) { // <- Epanalipsi oso yparxoun grammes sto CSV
                 String[] info = line.split ( cvsSplitBy ); // <- use ; as separator
 
                 //diavazei thn hmerominia apo thn thesh 2 tou pinaka
                 Date date = df.parse ( info[2] );
-                Date datenow = df.parse ( formattedDateNow );
 
                 String expir="EXPIRED"; // <- epanafora ths "expir" sthn arxikh ths timh
 
                 // An h hmerominia einai megalhterh apo thn shmerinh, tote einai VALID
-                if (date.compareTo ( datenow ) >= 0) {
+                if (date.compareTo ( dateNow ) >= 0) {
                     expir = "VALID";
                 }
 
 
                 // ektyposh olwn twn stoixeiwn gia dokimh
                 System.out.println ( "The owner is: " + info[1] + " the licence number is: " + info[0] +
-                        " and the date registered is: " +
-                        (new SimpleDateFormat ( "dd/MM/yyyy" ).format ( date )) + ". This licence is: " + expir );
+                        " and the date registered is: " + date + ". This licence is: " + expir );
 
             }
         }
