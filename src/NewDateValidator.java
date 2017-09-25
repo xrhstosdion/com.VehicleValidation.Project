9dbe23468d09a3f195f97eea9edcd46d415f6fa8
xrhstosdion <xrhstosdion@hotmail.com>
2017-09-25 19:20:33 +0300

    converting from Array to Table

--- src/NewDateValidator.java
+++ src/NewDateValidator.java
@@ -1,3 +1,5 @@
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -37,39 +39,44 @@ public class NewDateValidator {
         return dateNow;
     }
 
-    public String[] fileParser() {
-
+    public String[][] fileParser() {
+        String[] info = new String[3];
+        String[][] info1 = new String[100][3];
+        int i = 0;
         //Diavasma arxeiou CSV
-        String filePath = new File("VehiclesData.csv").getAbsolutePath();
 
+        String filePath = new File("VehiclesData.csv").getAbsolutePath();
         String line = ""; // <- Diaxorizei thn kathe grammh sto CSV arxeio
 
         //Epanalipsh gia na apothikevei se pinaka tis times apo to CSV
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
             while ((line = br.readLine()) != null) { // <- Epanalipsi oso yparxoun grammes sto CSV
-                String[] info = line.split(";"); // <- use ; as separator
-
-
-
+                info = line.split(";"); // <- use ; as separator
+                for (int j= 0; j<3; j++){
+                info1[i][j] = info[j];
+                }
+                i++;
             }
         }
-
         // Aparaithto se periptosh pou dothei lathos hmeromhnia
         catch (IOException  e) {
             // Invalid date was entered
             e.printStackTrace();
         }
-        return fileParser();
+        return info1;
     }
-
 
     public NewDateValidator() {
 
-
+        String[][] info1 = fileParser();
+        String info5 = info1[0][2];
         //diavazei thn hmerominia apo thn thesh 2 tou pinaka
-        Date date = dateFormat().parse(info[2]);
-
+        Date date = null;
+        try {
+            date = dateFormat().parse(info5);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         String expiredStatus = "EXPIRED"; // <- epanafora ths "expir" sthn arxikh ths timh
 
         // An h hmerominia einai megalhterh apo thn shmerinh, tote einai VALID
@@ -79,9 +86,10 @@ public class NewDateValidator {
         String formatedDate = new SimpleDateFormat("dd/MM/yyyy").format(date);
 
         // ektyposh olwn twn stoixeiwn gia dokimh
-        System.out.println("The owner is: " + info[1] + " the licence number is: " + info[0] +
-                " and the date registered is: " + formatedDate + ". This licence is: " + expiredStatus);
+        System.out.println("The owner is: " + info1[0][1] + " the licence number is: " + info1[0][0] +
+                " and the date registered is: " + info1[0][2] + ". This licence is: " + expiredStatus);
     }
+
 
 }
 
